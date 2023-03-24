250c69f1d674cc77559c87ce68a7871d7e29cd4a
Stefano <ste.amedeo@gmail.com>
2023-03-24 10:44:54 +0100

    Updated PO and Wave Quarter info

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
-# pcards
+# Print Physical Card
 
-Print work items in a format great for agile/scrum boards!
+This extension adds the ability to print cards to use on a physcial Kanban board. The extension adds an action to the context menu for Work Items so that it is available
+within your Boards, Work Item lists, query results, or Work Item view from the context menu drop down when you click the elipsis (...).
 
-## Notes
+This will allow you to print ANY Work Item type, from ANY template, even custom templates or work items. Some work item types may make more sense to print than others... So printing a User Story, or Feature or Epic etc... work well.. Where printing a Test Case's Share Step work item, while technically works, may not provide anything valuable.
 
-See details.md
+This Extension supports multi-selecting Work Items, and will print multiple cards to a page and will properly Page-Break for every three cards. You can multi-select different Work Item Types (User Stories and Tasks for example) to print out cards as needed.
 
-## How To Contribute
+## Printing
 
-1. Install Node
-2. `npm install -g webpack tfx-cli typings` to install global tools
-3. Clone the repository
-4. `npm install`
-5. Build with `npm run build` and upload to your VSTS
+- Open a work item, click the elipsis (...), click `Print Physical Card` OR
+- Right-click on a work item and select `Print Physical Card` OR
+- Multi-select work items (Ctrl-select or shift-select multiple items) right click and select `Print Physical Card`
+
+![Print card](static/img/pretty_card.png)
+![Print multiple cards](static/img/pretty_multiple.png)
+
+## Fields Printed
+
+- Work Item Type
+- Work Item ID Number
+- Work Item Title
+- Assigned To
+- Story Point / Business Value / Original Estimate (depends on work item type)
+- Area
+- Iteration
+- Tags
+
+![Printing Multple Cards](static/img/card-print.png)
+
+## Credits
+
+- Based on [Pretty Card](https://github.com/ryanjones/pcards) by Ryan Jones and
+  [WIPrint](https://github.com/MrTarantula/WIPrint)
 
 ## Credits
 
--- src/pcards.ts
+++ src/pcards.ts
@@ -38,15 +38,15 @@ const printWorkItems = {
             actionContext.ids || [actionContext.workItemId || actionContext.id];
 
           return getWorkItems(wids)
-            .then(workItems => prepare(workItems))
-            .then(pages => {
+            .then((workItems) => prepare(workItems))
+            .then((pages) => {
               return Q.all(pages);
             })
             .then((pages: any) => {
               const workItems = document.createElement("div");
               workItems.setAttribute("class", "container border");
               let wiCardCount = 0;
-              pages.forEach(page => {
+              pages.forEach((page) => {
                 let bugCard: any;
                 let userStoryCard: any;
                 let taskCard: any;
@@ -61,19 +61,22 @@ const printWorkItems = {
                     estimate: page.estimate,
                     assigned_to: page.assigned_to,
                     area_path: page.area_path,
+                    wave_quarter: page.wave_quarter,
+                    product_owner: page.product_owner,
                     iteration_path: page.iteration_path,
                     tags: page.tags,
                     border_color: page.border_color,
-                    icon: page.icon
+                    icon: page.icon,
                   });
                   workItems.innerHTML += userStoryCard;
-                }
-                else {
-                  workItems.innerHTML  += "<div> ERROR <br>" + page.message + "</div>";
+                } else {
+                  workItems.innerHTML +=
+                    "<div> ERROR <br>" + page.message + "</div>";
                 }
 
-                if ((wiCardCount % 3) === 0 && pages.length > wiCardCount) {
-                  workItems.innerHTML += "<p style='page-break-before: always'><br/>&nbsp;<br/>";
+                if (wiCardCount % 3 === 0 && pages.length > wiCardCount) {
+                  workItems.innerHTML +=
+                    "<p style='page-break-before: always'><br/>&nbsp;<br/>";
                 }
               });
               document.body.appendChild(workItems);
@@ -90,10 +93,10 @@ const printWorkItems = {
         },
         icon: "static/img/print14.png",
         text: menuItemText,
-        title: menuItemText
-      } as IContributedMenuItem
+        title: menuItemText,
+      } as IContributedMenuItem,
     ];
-  }
+  },
 };
 
 // Promises
@@ -106,159 +109,168 @@ function getWorkItems(wids: number[]): IPromise<Models.WorkItem[]> {
   );
 }
 
-function getWorkItemDefinition(thisWorkItem: Models.WorkItem): IPromise<Models.WorkItemType> {
-  return client.getWorkItemType(thisWorkItem.fields["System.TeamProject"], thisWorkItem.fields["System.WorkItemType"]);
+function getWorkItemDefinition(
+  thisWorkItem: Models.WorkItem
+): IPromise<Models.WorkItemType> {
+  return client.getWorkItemType(
+    thisWorkItem.fields["System.TeamProject"],
+    thisWorkItem.fields["System.WorkItemType"]
+  );
 }
 
 function getLastPathValue(pathText: string): string {
   if (pathText.length > 0) {
     let pathArray: string[] = pathText.split("\\");
     return pathArray[pathArray.length - 1];
-  }
-  else {
+  } else {
     return pathText;
   }
 }
 
 function prepare(workItems: Models.WorkItem[]) {
-  return workItems.map(item => {
+  return workItems.map((item) => {
     let result = {};
 
-    return getWorkItemDefinition(item).then(thisWIT => {
+    return getWorkItemDefinition(item).then((thisWIT) => {
+      console.log("PO: ", item.fields["Custom.ProductOwnerIVECO"]);
+      console.log("wave: ", item.fields["Custom.WAVEQUARTER"]);
       try {
         let template_filled: boolean = false;
         let work_item_color = thisWIT["color"];
         let work_item_icon = thisWIT.icon["url"];
         let tag_val = item.fields["System.Tags"];
         let area_val = getLastPathValue(item.fields["System.AreaPath"]);
-        let iteration_val = getLastPathValue(item.fields["System.IterationPath"]);
+        let iteration_val = getLastPathValue(
+          item.fields["System.IterationPath"]
+        );
         if (item.fields["System.WorkItemType"] === "User Story") {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Scheduling.StoryPoints"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.StoryPoints"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
 
         if (item.fields["System.WorkItemType"] === "Product Backlog Item") {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Common.BusinessValue"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Common.BusinessValue"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
 
         if (item.fields["System.WorkItemType"] === "Requirement") {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
 
         if (item.fields["System.WorkItemType"] === "Bug") {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
 
         if (item.fields["System.WorkItemType"] === "Task") {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "description":  item.fields["System.Description"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            description: item.fields["System.Description"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
-        if (item.fields["System.WorkItemType"] === "Epic" || item.fields["System.WorkItemType"] === "Feature") {
+        if (
+          item.fields["System.WorkItemType"] === "Epic" ||
+          item.fields["System.WorkItemType"] === "Feature"
+        ) {
           result = {
-            "type": item.fields["System.WorkItemType"],
-            "title": item.fields["System.Title"],
-            "description":  item.fields["System.Description"],
-            "id":  item.fields["System.Id"],
-            "estimate" : item.fields["Microsoft.VSTS.Common.BusinessValue"],
-            "assigned_to": item.fields["System.AssignedTo"],
-            "area_path": area_val,
-            "iteration_path": iteration_val,
-            "tags": tag_val,
-            "border_color": work_item_color,
-            "icon": work_item_icon
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            description: item.fields["System.Description"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.Effort"],
+            assigned_to: item.fields["System.AssignedTo"],
+            product_owner: item.fields["Custom.ProductOwnerIVECO"],
+            wave_quarter: item.fields["Custom.WAVEQUARTER"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
           };
           template_filled = true;
         }
         if (!template_filled) {
           result = {
-          "type": item.fields["System.WorkItemType"],
-          "title": item.fields["System.Title"],
-          "description":  item.fields["System.Description"],
-          "id":  item.fields["System.Id"],
-          "estimate" : item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
-          "assigned_to": item.fields["System.AssignedTo"],
-          "area_path": area_val,
-          "iteration_path": iteration_val,
-          "tags": tag_val,
-          "border_color": work_item_color,
-          "icon": work_item_icon
-        };
+            type: item.fields["System.WorkItemType"],
+            title: item.fields["System.Title"],
+            description: item.fields["System.Description"],
+            id: item.fields["System.Id"],
+            estimate: item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"],
+            assigned_to: item.fields["System.AssignedTo"],
+            area_path: area_val,
+            iteration_path: iteration_val,
+            tags: tag_val,
+            border_color: work_item_color,
+            icon: work_item_icon,
+          };
         }
+      } catch (e) {
+        result = {
+          type: "processerror",
+          message: e,
+        };
       }
-    catch (e) {
-      result = {
-        "type": "processerror",
-        "message": e
-      };
-    }
-    return result;
+      return result;
+    });
   });
-  });
-
 }
 
 VSS.register(
-  `${extensionContext.publisherId}.${
-    extensionContext.extensionId
-  }.print-work-item`,
+  `${extensionContext.publisherId}.${extensionContext.extensionId}.print-work-item`,
   printWorkItems
 );
